8b56ab13556e24ac8a2f8607505728155e564d62
ahmad <ahmadfarhn14@gmail.com>
2026-06-08 19:59:03 +0700

    fix: vite config for production

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite'; // <-- Tambahkan baris ini
 
 export default defineConfig({
     plugins: [
-        tailwindcss(), // <-- Tambahkan fungsi ini di atas laravel plugin
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
