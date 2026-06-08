b204cdb00b919496970f20da9e093cbef5c2142f
ahmad <ahmadfarhn14@gmail.com>
2026-06-08 20:24:03 +0700

    fix: secure vite server protocol and setup tailwind v4

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    // TAMBAHKAN BLOK SERVER INI DI BAWAH PLUGINS AGAR WEBSOCKET VITE BERJALAN AMAN VIA HTTPS
+    server: {
+        hmr: {
+            protocol: 'wss',
+        },
+    },
 });
